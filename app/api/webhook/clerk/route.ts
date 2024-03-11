a5c76e8d3959fbfe0467c5776dddf4e13af674a7
whiHak <betse1111111111@gmail.com>
2024-03-11 11:00:36 +0300

    ...

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,3 +1,4 @@
+"use client"
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
@@ -53,7 +54,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
- 
+ console.log(evt.data)
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 

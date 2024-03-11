70e07f665d258d013eb945d5af375caad991db6a
whiHak <betse1111111111@gmail.com>
2024-03-11 10:12:18 +0300

    added event action and uploadThing

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -24,6 +24,9 @@ import Image from "next/image";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useRouter } from "next/navigation";
+import { createEvent } from "@/lib/actions/event.action";
 
 type EventFormProps = {
   userId: string;
@@ -33,6 +36,9 @@ type EventFormProps = {
 const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
   const initialValues = eventDefaultValues;
+  const router = useRouter();
+
+  const { startUpload } = useUploadThing("imageUploader");
 
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
@@ -40,10 +46,37 @@ const EventForm = ({ userId, type }: EventFormProps) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    const eventData = values;
+
+    let uploadedImageUrl = eventData.imageUrl;
+
+    if(files.length > 0 ){
+      const uploadedImages = await startUpload(files);
+
+      if(!uploadedImages){
+        return
+      }
+
+      uploadedImageUrl =  uploadedImages[0].url
+
+    }
+    if(type ==="Create"){
+      try {
+        const newEvent = await createEvent({
+          event: {...values, imageUrl: uploadedImageUrl},
+          userId,
+          path: '/profile'
+        });
+
+        if(newEvent){
+          form.reset();
+          router.push(`/events/${newEvent._id}`)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (

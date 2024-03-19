c3583dcc340b62b41df7a344b3b48c2a59d4b3dc
whiHak <betse1111111111@gmail.com>
2024-03-19 22:01:20 +0300

    added collections

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -5,7 +5,6 @@ const CreateEvent = () => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md-py-10">

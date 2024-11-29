cd2103a9bc817135adde7ea16b1d492188d89707
whiHak <betse1111111111@gmail.com>
2024-11-29 12:24:29 +0300

    fixed clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -56,33 +56,26 @@ export async function POST(req: Request) {
   console.log(eventType)
  
   if(eventType === 'user.created') {
-    // const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    // const user = {
-    //   clerkId: id,
-    //   email: email_addresses[0].email_address,
-    //   username: username!,
-    //   firstName: first_name,
-    //   lastName: last_name,
-    //   photo: image_url,
-    // }
     const user = {
-      clerkId: "id",
-      email: "email_addresses[0].email_address",
-      username: "username"!,
-      firstName: "first_name",
-      lastName: "last_name",
-      photo: "image_url",
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
+    
     const newUser = await createUser(user);
 
-    // if(newUser) {
-    //   await clerkClient.users.updateUserMetadata( id, {
-    //     publicMetadata: {
-    //       userId: newUser._id
-    //     }
-    //   })
-    // }
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata( id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
 
     return NextResponse.json({ message: 'OK', user: newUser })
   }
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clertId: { type: String, required: true, unique: true },
+  clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
